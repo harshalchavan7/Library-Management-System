1c1f19373b5f5637e8f3706a97507c5d802f1757
harshalchavan7 <harshalschavan2003@gmail.com>
2024-03-18 12:24:42 +0530

    Added Book.java

---
+++ src/main/java/com/librarysystem/model/Book.java
@@ -0,0 +1,61 @@
+package com.librarysystem.model;
+
+public class Book {
+    // Attributes
+    private String isbn;
+    private String title;
+    private String author;
+    private int yearPublished;
+
+    // Constructor
+    public Book(String isbn, String title, String author, int yearPublished) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    // Getters and setters
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    // toString method to represent Book object as a string
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", yearPublished=" + yearPublished +
+                '}';
+    }
+}
