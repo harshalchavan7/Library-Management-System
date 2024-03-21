645f979df77d4c596134436a938564d4fec399b2
harshalchavan7 <harshalschavan2003@gmail.com>
2024-03-21 15:03:45 +0530

    Added UserService.java

---
+++ src/main/java/com/librarysystem/service/UserService.java
@@ -0,0 +1,43 @@
+public class UserService {
+    private List<User> users;
+
+    public UserService() {
+        this.users = new ArrayList<>();
+    }
+
+    // Method to add a user
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    // Method to remove a user
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+
+    // Method to find a user by their ID
+    public User findUserById(int userId) {
+        for (User user : users) {
+            if (user.getId() == userId) {
+                return user;
+            }
+        }
+        return null; // User not found
+    }
+
+    // Method to find users by their name
+    public List<User> findUsersByName(String name) {
+        List<User> foundUsers = new ArrayList<>();
+        for (User user : users) {
+            if (user.getName().equals(name)) {
+                foundUsers.add(user);
+            }
+        }
+        return foundUsers;
+    }
+
+    // Method to get all users
+    public List<User> getAllUsers() {
+        return users;
+    }
+}
