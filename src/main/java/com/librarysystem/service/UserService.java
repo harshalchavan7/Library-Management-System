ae2db59e8ef7c47e74893d66e85f8fda6d3c8b80
harshalchavan7 <harshalschavan2003@gmail.com>
2024-03-28 15:17:34 +0530

    Added UserService.java

--- src/main/java/com/librarysystem/service/UserService.java
+++ src/main/java/com/librarysystem/service/UserService.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
+    // Declare the 'users' list at the class level
     private List<User> users;
 
     public UserService() {
